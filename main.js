3b49d3de11ec2d6edffbf7c4faa101df4b7d35ce
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-10-11 16:26:10 +0200

    Change queston in JavaScript

---
+++ main.js
@@ -0,0 +1,5 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+const myQuestion = document.querySelector(".question");
+myQuestion.textContent = "Do you want a coffee break?";
